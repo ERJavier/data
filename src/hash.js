4ff6110c5f274374a2264fea9f1627206e37eafb
Javier Cepeda <erjavierc@gmail.com>
2022-08-22 20:35:58 -0400

    hash

--- src/hash.js
+++ src/hash.js
@@ -10,7 +10,15 @@ class HashTable {
         return hash;
     }
     set(key, value){
+        const address = this.hashMethod(key);
         
+        if(!this.data[address]) {
+            this.data[address] = []
+        }
+
+        this.data[address].push([key, value])
+        return this.data;
+    
     }
 }
 
