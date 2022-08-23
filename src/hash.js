bccf3afcfe42d9eec50e079277f2f13e4ea2818a
Javier Cepeda <erjavierc@gmail.com>
2022-08-22 20:48:20 -0400

    hash

--- src/hash.js
+++ src/hash.js
@@ -11,17 +11,26 @@ class HashTable {
     }
     set(key, value){
         const address = this.hashMethod(key);
-        
+
         if(!this.data[address]) {
             this.data[address] = []
         }
 
         this.data[address].push([key, value])
         return this.data;
-    
     }
-}
+    get(key) {
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+            if (currentBucket) {
+                for (let i = 0; i < currentBucket.length; i++) {
+                    if (currentBucket[i][0] === key) {
+                        return currentBucket[i][1];
+                    }
+                }
+            }
+        return undefined;
+        }
+    }
 
 const myHashTable = new HashTable(50);
-
-console.log(myHashTable)
